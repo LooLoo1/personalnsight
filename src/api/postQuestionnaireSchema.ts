6a183e5cb1900030a5ef7193f111faadc325710c
Vitalii Petriv <vitaliy.petriv.work@gmail.com>
2024-05-06 06:56:32 +0300

    Settings for test deployment with fake data without server involvement

--- src/api/postQuestionnaireSchema.ts
+++ src/api/postQuestionnaireSchema.ts
@@ -2,16 +2,16 @@ import { Choice } from 'types';
 
 export const postQuestionnaireSchema = async (requestData: Choice[]): Promise<Choice[]> => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/questionSchema`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(requestData),
-    });
-    const data = await res.json();
-    return data;
-    // return requestData;
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/questionSchema`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(requestData),
+    // });
+    // const data = await res.json();
+    // return data;
+    return requestData;
   } catch (error) {
     throw new Error('Failed to post questionnaire schema');
   }
